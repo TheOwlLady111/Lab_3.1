200d0c56ef08215806634538bb78076fc2ddfbae
Anashkevich Katherina <sweetsyammy2003@gmail.com>
2022-04-07 00:05:49 +0300

    Добавьте файлы проекта.

---
+++ Lab_3.1/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Lab_3._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnGenerate1 = new System.Windows.Forms.Button();
+            this.dataBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnGenerate2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(2, 4);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(583, 352);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // btnGenerate1
+            // 
+            this.btnGenerate1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGenerate1.Location = new System.Drawing.Point(834, 149);
+            this.btnGenerate1.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGenerate1.Name = "btnGenerate1";
+            this.btnGenerate1.Size = new System.Drawing.Size(172, 57);
+            this.btnGenerate1.TabIndex = 2;
+            this.btnGenerate1.Text = "Create(amount)";
+            this.btnGenerate1.UseVisualStyleBackColor = true;
+            this.btnGenerate1.Click += new System.EventHandler(this.btnGenerate1_Click);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(2, 364);
+            this.chart2.Margin = new System.Windows.Forms.Padding(4);
+            this.chart2.Name = "chart2";
+            this.chart2.Size = new System.Drawing.Size(583, 370);
+            this.chart2.TabIndex = 3;
+            this.chart2.Text = "chart2";
+            // 
+            // btnGenerate2
+            // 
+            this.btnGenerate2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGenerate2.Location = new System.Drawing.Point(834, 540);
+            this.btnGenerate2.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGenerate2.Name = "btnGenerate2";
+            this.btnGenerate2.Size = new System.Drawing.Size(172, 57);
+            this.btnGenerate2.TabIndex = 4;
+            this.btnGenerate2.Text = "Create(complexity)";
+            this.btnGenerate2.UseVisualStyleBackColor = true;
+            this.btnGenerate2.Click += new System.EventHandler(this.btnGenerate2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(641, 100);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 53);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Amount of elements in array";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(641, 209);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(175, 53);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Complexity";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(637, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 35);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Elements in array:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(641, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 38);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Complexity";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(641, 484);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(175, 55);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "Amount of elements in array";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(641, 600);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(175, 56);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "Complexity";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(641, 444);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(193, 37);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Elements in array:";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(641, 556);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(175, 41);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Complexity";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 794);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnGenerate2);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.btnGenerate1);
+            this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnGenerate1;
+        private System.Windows.Forms.BindingSource dataBindingSource;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button btnGenerate2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lab_3.1/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lab_3._1
+{
+    public partial class Form1 : Form
+    {
+        const int MIN = 0;
+        const int MAX = 20;
+        int _arrayLenght = 100;
+        int _complexity = 10000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public int[] CreateArray(int size)
+        {
+            Random random = new Random();
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(MIN, MAX);
+            }
+
+            return array;
+        }
+
+        public void GetThreadsWorkTime(int threadCount, int arrayLeangth, int key, int complexity)
+        {
+            Stopwatch time = new Stopwatch();
+            Barrier barrier = new Barrier(threadCount, b =>
+            {
+                if (key != -1)
+                {
+                    time.Stop();
+                    Action action = () => { chart1.Series[1].Points.AddXY(threadCount, time.Elapsed.TotalSeconds); };
+                    chart1.Invoke(action);
+                }
+                    
+            });
+
+            int[] arrayA = CreateArray(arrayLeangth);
+            double[] arrayB = new double[arrayLeangth];
+            time.Start();
+            for (int j = 0; j < threadCount; j++)
+            {
+                int x = j;
+                int start = arrayA.Length * x / threadCount;
+                int end = arrayA.Length * (x + 1) / threadCount;
+                Thread thread = new Thread(() =>
+                {
+                    StartThreadsMathPow(arrayLeangth, arrayA, arrayB, complexity, start, end, ref key, barrier);
+                });
+                thread.Start();
+
+            }
+            Thread.Sleep(100);
+        }
+
+        public void GetThreadsWorkTime(int threadCount, int arrayLeangth, int key, int complexity, int num)
+        {
+            Stopwatch time = new Stopwatch();
+            Barrier barrier = new Barrier(threadCount, b =>
+            {
+                if (key != -1)
+                {
+                    time.Stop();
+                    Console.WriteLine(time.Elapsed.TotalSeconds.ToString());
+                    Action action = () => { chart2.Series[num].Points.AddXY(complexity, time.Elapsed.TotalSeconds); };
+                    chart2.Invoke(action);
+                }
+            });
+
+            int[] arrayA = CreateArray(arrayLeangth);
+            double[] arrayB = new double[arrayLeangth];
+            time.Start();
+            for (int j = 0; j < threadCount; j++)
+            {
+                int x = j;
+                int start = arrayA.Length * x / threadCount;
+                int end = arrayA.Length * (x + 1) / threadCount;
+                Thread thread = new Thread(() =>
+                {
+                    StartThreadsMathPow(arrayLeangth, arrayA, arrayB, complexity, start, end, ref key, barrier);
+                });
+                thread.Start();
+            }
+            Thread.Sleep(100);
+        }
+
+        public void MathPow(int start, int end, int[] arrayA, double[] arrayB, int k)
+        {
+            for (int i = start; i < end; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    arrayB[i] += Math.Pow(arrayA[i], 1.789);
+                }
+            }
+            
+        }
+
+        public void StartThreadsMathPow(int arrayLength, int[] arrayA, double[] arrayB, int k, int start, int end, ref int key, Barrier barrier)
+        {
+            barrier.SignalAndWait();
+            Interlocked.Increment(ref key);
+            MathPow(start, end, arrayA, arrayB, k);
+            barrier.SignalAndWait();
+        }
+
+        private void btnGenerate1_Click(object sender, EventArgs e)
+        {
+            _arrayLenght = Convert.ToInt32(textBox1.Text);
+            _complexity = Convert.ToInt32(textBox2.Text);
+
+            var objChart = chart1.ChartAreas[0];
+            objChart.AxisX.IntervalType = DateTimeIntervalType.Number;
+            objChart.AxisX.Minimum = 0;
+            objChart.AxisX.Maximum = 30;
+
+            objChart.AxisY.IntervalType = DateTimeIntervalType.Number;
+            objChart.AxisY.Minimum = 0;
+            objChart.AxisY.Maximum = 1;
+
+            chart1.Series.Clear();
+            chart1.Series.Add("Without threads").Color = Color.DarkBlue;
+            chart1.Series.Add("With threads").Color = Color.DarkMagenta;
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+            chart1.Series[1].ChartType = SeriesChartType.Line;
+
+            int[] a = CreateArray(_arrayLenght);
+            double[] b = new double[_arrayLenght];
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+                MathPow(0, _arrayLenght, a, b, _complexity);
+            sw.Stop();
+            //Thread.Sleep(10000);
+            chart1.Series[0].Points.AddXY(0, sw.Elapsed.TotalSeconds);
+            chart1.Series[0].Points.AddXY(30, sw.Elapsed.TotalSeconds);
+            for (int j = 2; j <= 30; j += 2)
+            {
+                GetThreadsWorkTime(j, _arrayLenght, -1, _complexity);
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void btnGenerate2_Click(object sender, EventArgs e)
+        {
+            _arrayLenght = Convert.ToInt32(textBox3.Text);
+            _complexity = Convert.ToInt32(textBox4.Text);
+
+            var objChart = chart2.ChartAreas[0];
+            objChart.AxisX.IntervalType = DateTimeIntervalType.Number;
+            objChart.AxisX.Minimum = 0;
+            objChart.AxisX.Maximum = _complexity;
+
+            objChart.AxisY.IntervalType = DateTimeIntervalType.Number;
+            objChart.AxisY.Minimum = 0;
+            objChart.AxisY.Maximum = 1;
+
+            chart2.Series.Clear();
+            chart2.Series.Add("Time from K - 1 threads").Color = Color.BlueViolet;
+            chart2.Series[0].ChartType = SeriesChartType.Line;
+            chart2.Series.Add("Time from K - 4 threads").Color = Color.Blue;
+            chart2.Series[1].ChartType = SeriesChartType.Line;
+            chart2.Series.Add("Time from K - 12 threads").Color = Color.DarkCyan;
+            chart2.Series[2].ChartType = SeriesChartType.Line;
+            chart2.Series.Add("Time from K - 24 threads").Color = Color.Green;
+            chart2.Series[3].ChartType = SeriesChartType.Line;
+            chart2.Series.Add("Time from K - 50 threads").Color = Color.Orange;
+            chart2.Series[4].ChartType = SeriesChartType.Line;
+
+            for (int j = 100; j <= _complexity; j+=100)
+            {
+                GetThreadsWorkTime(1, _arrayLenght, -1, j, 0);
+                Thread.Sleep(1000);
+                GetThreadsWorkTime(5, _arrayLenght, -1, j, 1);
+                Thread.Sleep(1000);
+                GetThreadsWorkTime(15, _arrayLenght, -1, j, 2);
+                Thread.Sleep(1000);
+                GetThreadsWorkTime(25, _arrayLenght, -1, j, 3);
+                Thread.Sleep(100);
+                GetThreadsWorkTime(30, _arrayLenght, -1, j, 4);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
